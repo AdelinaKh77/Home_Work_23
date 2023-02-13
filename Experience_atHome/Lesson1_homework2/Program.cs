1f548d49fa9719baae29a08b1ff156a651993a23
Emil Ming <h.adelina772gmail.com>
2023-02-13 10:12:59 +0300

    Задача №4

---
+++ Experience_atHome/Lesson1_homework2/Program.cs
@@ -0,0 +1,36 @@
+﻿//Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+
+double a;
+double b;
+double c;
+double max;
+
+
+a=44;
+b=5;
+c=78;
+max=0;
+
+if (a>b)
+{
+    if (a>c)
+    {
+    Console.WriteLine (max=a);
+    }
+    else
+    {
+    Console.WriteLine (max=c);
+    }
+}
+else
+{
+    if (b>c)
+    {
+    Console.WriteLine (max=b);
+    }
+    else
+    {
+    Console.WriteLine(max=c);
+    }
+}
