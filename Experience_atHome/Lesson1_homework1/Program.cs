1153130ed661c0bb22b04aedc9729c6d326a8300
Emil Ming <h.adelina772gmail.com>
2023-02-13 10:05:03 +0300

    Задача №2

---
+++ Experience_atHome/Lesson1_homework1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+
+
+double a;
+double b;
+
+a = -9;
+b = -3;
+
+if (a>b)
+{
+Console.WriteLine("a=max b=min");
+}
+else
+{
+Console.WriteLine("b=max a=min");
+}
