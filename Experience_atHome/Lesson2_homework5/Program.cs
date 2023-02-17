2e92414a125bdc0583f10f52718f34dcced4dbde
Emil Ming <h.adelina772gmail.com>
2023-02-17 18:40:07 +0300

    Домашняя работа лекция2 три задачи

---
+++ Experience_atHome/Lesson2_homework5/Program.cs
@@ -0,0 +1,44 @@
+﻿// 0. Вывести квадрат числа
+
+//double x;
+//x=25;
+
+//double result;
+//result = x*x;
+
+//Console.WriteLine(result);
+
+// 1. По двум заданным числам проверять является ли первое квадратом второго 
+
+//double x;
+//double y;
+
+//x = 9; y = 3;
+
+//if (x == y * y)
+//{
+    //Console.WriteLine("Да");
+//}
+//else
+//{
+    //Console.WriteLine("Нет");  
+//}
+
+// 3. Дано число. Проверить кратно ли оно 7 и 23
+
+
+
+int number = 161;
+
+int a = 7;
+int b = 23;
+string txtResult = " ";
+
+if (number % a == 0 && number % b == 0) 
+{
+ Console.WriteLine (txtResult = "кратно обоим числам");
+} 
+ else
+ {
+ Console.WriteLine (txtResult = "условие не выполнено");
+ }
