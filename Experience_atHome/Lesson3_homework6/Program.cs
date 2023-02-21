724aa1eab1f5bfa156bf6599074f12cdca95d854
Adelina Kharisova <h.adelina772gmail.com>
2023-02-21 18:18:07 +0300

    Задание 1 к семинару 3

---
+++ Experience_atHome/Lesson3_homework6/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+//14212 -> нет
+
+//12821 -> да
+
+//23432 -> да
+
+ 
+ Console.WriteLine("Введите пятизначное число: ");
+ string input=Console.ReadLine();
+ int value = Convert.ToInt32(input);
+ int a,b,c,d,e;
+ int result;
+
+ result = (value/10000);
+ a=result;
+ result = value%10;
+ e=result;
+ if (a==e)
+ {
+    result=(value%10000)/1000;
+    b=result;
+    result=(value%100)/10;
+    d=result;
+    if (d==b)
+    {
+        Console.WriteLine("Данное число-Полиндром");
+    }
+    else
+    {
+        Console.WriteLine("Данное число не является Полиндромом"); 
+    }
+ }
+else
+{
+    Console.WriteLine("Данное число не является Полиндромом");
+}
+
+
