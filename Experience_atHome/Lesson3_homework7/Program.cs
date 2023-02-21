c2159e1943b037b752f5b1bb2f08040695814743
Adelina Kharisova <h.adelina772gmail.com>
2023-02-21 20:36:06 +0300

    Задание 3 к семинару 3

---
+++ Experience_atHome/Lesson3_homework7/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+
+
+Console.WriteLine("Введите любое целое число");
+string input = Console.ReadLine();
+int N = Convert.ToInt32(input);
+
+int a = 1;
+int result;
+while (a <= N)
+    {
+     result = a*a*a;
+     Console.WriteLine(result);
+     a++;
+    }
+
+   Console.WriteLine("вычисление окончено");
+
+    
+
