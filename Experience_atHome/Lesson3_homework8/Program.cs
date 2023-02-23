95b7f40dc67cb67f2252c17a357bf6d2aab4963a
Adelina Kharisova <h.adelina772gmail.com>
2023-02-23 18:03:58 +0300

    Задача №2 к 3 семинару

---
+++ Experience_atHome/Lesson3_homework8/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int [] array={3,6,8,2,1,-7};
+int  sum1=(array [3]-array[0])*(array [3]-array[0]);
+int  sum2=(array [4]-array [1])*(array [4]-array [1]);
+int  sum3= (array [5]-array[2])*(array [5]-array[2]);
+double sum=sum1+sum2+sum3;
+double result = Math.Sqrt(sum);
+Console.WriteLine(result);
