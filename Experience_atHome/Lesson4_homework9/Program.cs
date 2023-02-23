14a08ccbbf30c183aa871ab3353f9557f983f511
Adelina Kharisova <h.adelina772gmail.com>
2023-02-23 19:46:33 +0300

    Задача 25 к лекции 4

---
+++ Experience_atHome/Lesson4_homework9/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 25: Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//Math.Pow использовать нельзя
+//3, 5 -> 243 (3⁵)
+
+//2, 4 -> 16
+
+Console.WriteLine( "Введите  число:  ");
+int value = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine( "Введите степень: ");
+int power = Convert.ToInt32(Console.ReadLine());
+
+int result = value;
+for (int i = 1; i<power; i++)
+{
+value = result*value;
+}
+Console.WriteLine(value);
+
+
+
+
