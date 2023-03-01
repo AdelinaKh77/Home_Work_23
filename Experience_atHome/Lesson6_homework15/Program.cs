f389fd95637e75dc9be7b2418c29fe5c5540d567
Adelina Kharisova <h.adelina772gmail.com>
2023-03-01 18:37:49 +0300

    Задача 41 к 6 лекции

---
+++ Experience_atHome/Lesson6_homework15/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3/
+
+System.Console.Write("Какое количество чисел будет проверяться: "); 
+ int number= int.Parse(Console.ReadLine()); 
+int[] array = new int[number]; 
+ 
+for (int i = 0; i < array.Length; i++) 
+{ 
+System.Console.WriteLine($"Заполните ячейки числами {i}: "); 
+array[i]= int.Parse(System.Console.ReadLine());  
+} 
+int sum = 0; 
+for (int i = 0; i < array.Length; i++) 
+    { 
+        if (array[i] > 0) sum += 1; 
+    } 
+System.Console.WriteLine($"В массиве {sum} числа больше нуля.");
