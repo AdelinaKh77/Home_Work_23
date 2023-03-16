4eaed9c830c70ec4e68d6cecc4a33a8ff9e01e8a
Adelina Kharisova <h.adelina772gmail.com>
2023-03-16 20:30:43 +0300

    Заключение. Задача 64

---
+++ Experience_atHome/Lesson8_homework21/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задайте значения N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+
+/*Console.WriteLine ("Введите число N:");
+int N =  int.Parse(Console.ReadLine());
+return4*/
+
+Console.Clear();
+
+
+int EnterMessage(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+bool ValidNum(int num) => num > 0;
+
+string ReturnNumber(int Num)
+{
+    if (Num >= 1)
+    {
+        return $"{Num} " + ReturnNumber(Num - 1);
+    }
+    else
+    {
+        return String.Empty;
+    }
+}
+
+int num = EnterMessage("Введите число : ");
+
+if (ValidNum(num))
+{
+    Console.WriteLine(ReturnNumber(num));
+}
+else
+{
+    Console.WriteLine("Ошибка, попробуйте снова !");
+}
